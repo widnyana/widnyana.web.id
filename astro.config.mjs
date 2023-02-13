38499af8869ae70df426b1e8277d790c9ed7a3bc
widnyana <wid@widnyana.web.id>
2023-02-13 13:24:52 +0700

    feat: initial version
    Signed-off-by: widnyana <wid@widnyana.web.id>

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,21 @@
 import { defineConfig } from 'astro/config';
+import vercel from '@astrojs/vercel/static';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  publicDir: './public',
+  site: 'https://widnyana.web.id',
+  trailingSlash: 'ignore',
+  output: 'static',
+  adapter: vercel(),
+  build: {
+    format: 'directory'
+  },
+  markdown: {
+    drafts: false,
+    syntaxHighlight: 'shiki',
+    remarkPlugins: [
+      'remark-lint'
+    ]
+  },
+});
